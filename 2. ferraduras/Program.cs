6280417319474b1d45018890d85073bf829c6260
joaqn07 <j81340329@gmail.com>
2025-06-16 19:22:59 -0300

    Atualizar o Program.cs

--- 2. ferraduras/Program.cs
+++ 2. ferraduras/Program.cs
@@ -1,20 +1,7 @@
-﻿decimal terreno = 0, comprimento, largura;
+Console.Write("Digite a quantidade de cavalos comprados: ");
+int quantidadeCavalos = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(" diga o comprimento do terreno que gostaria de comprar ");
-comprimento = Convert.ToDecimal(Console.ReadLine());
+int ferradurasNecessarias = quantidadeCavalos * 4;
 
-Console.WriteLine("agora por favor me diga a largura");
-largura = Convert.ToDecimal(Console.ReadLine());
-
-if (comprimento != largura)
-{
-    terreno = comprimento * largura;
-    Console.WriteLine(" a area do terreno é " + terreno + " parabens ");
-
-}
-
-if (comprimento == largura)
-{
-
-    Console.WriteLine(" me desculpe, apenas vendemos terrenos retangulares");
-}
+Console.WriteLine("Serão necessárias " + ferradurasNecessarias + " ferraduras.");
+    
