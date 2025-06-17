3e79feb17b7ee0a1bd39b9d821c3205ba78c13d0
joaqn07 <j81340329@gmail.com>
2025-06-16 21:00:31 -0300

    Atualizar o Program.cs

--- 25. caixa dagua/Program.cs
+++ 25. caixa dagua/Program.cs
@@ -2,14 +2,14 @@
 decimal diametro, altura, raio, volumeCm3, volumeLitros;
 decimal pi = 3.1416m;
       
-        Console.Write("Informe o diâmetro da caixa d'água (em cm): ");
+Console.Write("Informe o diâmetro da caixa d'água (em cm): ");
         diametro = Convert.ToDecimal(Console.ReadLine());
 
-        Console.Write("Informe a altura da caixa d'água (em cm): ");
+Console.Write("Informe a altura da caixa d'água (em cm): ");
         altura = Convert.ToDecimal(Console.ReadLine());
 
        
-        raio = diametro / 2;
+raio = diametro / 2;
 
 
 volumeCm3 = pi * raio * raio * altura;
